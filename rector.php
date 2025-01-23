d6418c5af112b3988cb82042bb590249bb5f8fff
Ignacio El Kadre <imefisto@gmail.com>
2025-01-23 06:32:28 -0300

    chore: added rector

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([
+         LevelSetList::UP_TO_PHP_81
+    ])
+    ->withPreparedSets(deadCode: true);
